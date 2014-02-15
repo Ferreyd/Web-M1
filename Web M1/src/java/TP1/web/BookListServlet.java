5a4a4bcfebcf50bc50101bdba39ecc27c899b5d1
Ferreyd <cordina.nicolas@gmail.com>
2014-02-15 19:32:35 +0100

    Partie 1 du TP1 faite

---
+++ Web M1/src/java/TP1/web/BookListServlet.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package TP1.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Nicolas
+ */
+import java.io.IOException;  
+  
+import javax.inject.Inject;  
+import javax.servlet.ServletException;  
+import javax.servlet.annotation.WebServlet;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+  
+import TP1.BookRepository;  
+  
+@WebServlet("/book/")  
+public class BookListServlet extends HttpServlet {  
+      
+    @Inject   
+    private BookRepository bookRepo;  
+  
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {  
+        request.setAttribute("books", bookRepo.listBooks());  
+        getServletContext().getRequestDispatcher("/TP1/book-list.jsp").forward(request, response);  
+    }  
+  
+}  
---
+++ Web M1/web/TP1/book-list.jsp
@@ -0,0 +1,37 @@
+<%-- 
+    Document   : book-list.jsp
+    Created on : 14 févr. 2014, 18:07:04
+    Author     : Nicolas
+--%>
+
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>  
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix ="c" %>  
+<!DOCTYPE HTML>  
+  
+<html>  
+<head>  
+<title>Book listing</title>  
+</head>  
+<body>  
+  
+  
+<table>  
+    <tr>  
+        <th>Title</th>  
+        <th>Description</th>  
+        <th>Price</th>  
+        <th>Publication Date</th>  
+    </tr>  
+      
+    <c:forEach var="book" items="${books}">  
+        <tr>  
+            <td>${book.title}</td>  
+            <td>${book.description}</td>  
+            <td>${book.price}</td>  
+            <td>${book.pubDate}</td>  
+        </tr>  
+    </c:forEach>  
+</table>  
+  
+</body>  
+</html>  
